06e96a003d0b682ef2bf579fdeb89896a19d0c85
CODOUL Alexandre <axe.test1@gmail.com>
2018-09-08 12:09:16 +0200

    CleanImages Workinprogress

---
+++ Sources/CleanImages/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unbroken.LaunchBox.Plugins;
+using Unbroken.LaunchBox.Plugins.Data;
+using DxTrace;
+
+namespace CleanImages
+{
+    public class Main : IGameMenuItemPlugin
+    {
+        private string _App;
+
+        public Main()
+        {
+            _App = AppDomain.CurrentDomain.BaseDirectory;
+            DxTrace.InfoToFile logFile = new InfoToFile(@".\Logs\Clean_Images.log", false);
+
+            ITrace.AddListener(logFile);                        
+
+            ITrace.WriteLine($"\n {new string('=', 10)} Initialization {new string('=', 10)}");
+        }
+
+
+        public bool SupportsMultipleGames => true;
+
+        // Titre
+        public string Caption => Lang.Title_Menu;
+
+        public System.Drawing.Image IconImage => null;
+
+        public bool ShowInLaunchBox => true;
+
+        public bool ShowInBigBox => false;
+
+        public bool GetIsValidForGame(IGame selectedGame)
+        {
+            ITrace.WriteLine("GetIsValidForGames");
+            return true;
+          //  throw new NotImplementedException();
+        }
+
+        public bool GetIsValidForGames(IGame[] selectedGames)
+        {
+            ITrace.WriteLine("GetIsValidForGames[]");
+            return true;
+
+            // throw new NotImplementedException();
+        }
+
+        public void OnSelected(IGame selectedGame)
+        {
+            ITrace.WriteLine("On Selected");
+
+            //  throw new NotImplementedException();
+        }
+
+        public void OnSelected(IGame[] selectedGames)
+        {
+            ITrace.WriteLine("On Selected[]");
+
+            // throw new NotImplementedException();
+        }
+    }
+}
